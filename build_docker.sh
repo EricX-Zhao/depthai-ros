aaffd8a9ec3e1f33e9393fe06c582159ee80aeda
EricX <ericzhao0325@163.com>
2025-04-02 16:22:13 +0300

    feat: add stereo imu node

---
+++ build_docker.sh
@@ -0,0 +1,13 @@
+
+colcon build \
+    --merge-install \
+    --install-base depthai \
+    --cmake-force-configure \
+    --cmake-args \
+    --no-warn-unused-cli \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE=${RK3588_TOOLCHAIN_FILE} \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DBUILD_SHARED_LIBS=ON
