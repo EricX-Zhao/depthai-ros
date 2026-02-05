ce8c320825164ab4f123cc5812b78373e20bb547
EricX-Zhao <ericzhao0325@163.com>
2026-02-06 00:42:13 +0300

    use ros jazzy arm64 opencv

--- depthai-core-arm64-linux/share/depthai/local_setup.sh
+++ depthai-core-arm64-linux/share/depthai/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/depthai_ws/src/depthai-core/build_arm64/vcpkg_installed/arm64-linux"}
+: ${AMENT_CURRENT_PREFIX:="/workspace/src/depthai-core/build_arm64/vcpkg_installed/arm64-linux"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
